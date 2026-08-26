431967c4e8663d2f96431ea0da0260d4036cab22
rzupa88 <64094555+rzupa88@users.noreply.github.com>
2026-08-25 22:07:08 -0400

    Refresh cache for slider ratings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tinned-fish-guide-v2";
+const CACHE_NAME = "tinned-fish-guide-v3";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
